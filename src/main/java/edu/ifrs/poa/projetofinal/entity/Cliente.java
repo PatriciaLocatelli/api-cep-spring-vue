8d5b418e8ffd27526fca122b9747b8ae765e47e3
PatriciaLocatelli <patriciawalau@hotmail.com>
2019-12-10 21:23:17 -0300

    Inicializando o projeto

---
+++ src/main/java/edu/ifrs/poa/projetofinal/entity/Cliente.java
@@ -0,0 +1,33 @@
+package edu.ifrs.poa.projetofinal.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente{
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String nome;
+    private String telefone;
+    private String email;
+    private String rua;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String numero;
+    private String bairro;
+    private String localidade;
+    private String uf;
+
+}
